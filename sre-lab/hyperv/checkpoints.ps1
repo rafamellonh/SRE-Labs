31a3c7cc418bdd56084d241050beecfa95d04bcd
Rafael <56116868+rafamellonh@users.noreply.github.com>
2026-09-01 21:19:15 -0400

    Delete sre-lab directory

--- sre-lab/hyperv/checkpoints.ps1
+++
@@ -1,7 +0,0 @@
-# Snapshots das VMs. Restaurar leva segundos; refazer leva horas.
-# Uso: .\checkpoints.ps1 -Name 'antes-do-caos'
-
-param([Parameter(Mandatory=$true)][string]$Name)
-
-Get-VM | Checkpoint-VM -SnapshotName $Name
-Get-VMSnapshot -VMName * | Select-Object VMName, Name, CreationTime
